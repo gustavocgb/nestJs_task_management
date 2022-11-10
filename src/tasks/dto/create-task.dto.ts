ad7749405bef5ab5ea6743c831b2ef87adb69978
Gustavo Barbosa | Eva Benefícios <gustavo.barbosa@evacard.com.br>
2022-11-10 08:10:31 -0300

    feat: add pipe IsNoEmpty in task dto

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,9 @@
-// Data Transfer Object (DTO)
+import {IsNotEmpty} from "class-validator";
+
 export class CreateTaskDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
 }
