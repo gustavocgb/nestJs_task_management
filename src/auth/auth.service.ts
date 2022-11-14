84a9ee904b8ae4322a1f38a8d74fb0c930ac76ff
Gustavo Barbosa | Eva Benefícios <gustavo.barbosa@evacard.com.br>
2022-11-14 16:09:30 -0300

    feat: auth module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import {UsersRepository} from "./users.repository";
+import {InjectRepository} from "@nestjs/typeorm";
+
+@Injectable()
+export class AuthService {
+    constructor(
+        @InjectRepository(UsersRepository)
+       private userRepository: UsersRepository
+    ) {}
+
+
+
+}
