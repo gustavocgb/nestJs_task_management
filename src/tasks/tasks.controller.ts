c6e9776ea7b834142340846f66cb08141be1641d
Gustavo Barbosa | Eva Benefícios <gustavo.barbosa@evacard.com.br>
2022-11-14 14:35:57 -0300

    feat: createTask repository

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -4,9 +4,19 @@ import {TaskStatus} from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 import {GetTasksFilterDto} from "./dto/get-tasks-filter.dto";
 import {UpdateTaskStatusDto} from "./dto/update-task-status.dto";
+import {Task} from "./task.entity";
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Promise<Task> {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.tasksService.createTask(createTaskDto);
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,9 +1,29 @@
 import {Injectable, NotFoundException} from '@nestjs/common';
-import { TaskStatus } from './task-status.enum';
-import { CreateTaskDto } from './dto/create-task.dto';
-import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
+import {TaskStatus} from './task-status.enum';
+import {CreateTaskDto} from './dto/create-task.dto';
+import {TasksRepository} from "./tasks.repository";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Task} from "./task.entity";
 
 @Injectable()
 export class TasksService {
+    constructor(
+        @InjectRepository(TasksRepository)
+        private taskRepository: TasksRepository
+    ) {}
+
+    async getTaskById(id: string): Promise<Task> {
+        console.log(id)
+        const found = await this.taskRepository.findOne({where: {id: id}})
+        console.log(found)
+        if (!found) {
+            throw new NotFoundException(`Task with ID ${id} not found`);
+        }
+        return found;
+    }
+
+    async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+        return await this.taskRepository.createTask(createTaskDto);
+    }
 
 }
