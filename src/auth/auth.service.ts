15dc64af0aed667c17eeee399103f4efb8b0c723
Gustavo Barbosa | Eva Benefícios <gustavo.barbosa@evacard.com.br>
2022-11-14 17:06:51 -0300

    feat: signup repository

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import {UsersRepository} from "./users.repository";
 import {InjectRepository} from "@nestjs/typeorm";
+import {AuthCredentialsDto} from "./dto/auth-credentials.dto";
 
 @Injectable()
 export class AuthService {
@@ -9,6 +10,8 @@ export class AuthService {
        private userRepository: UsersRepository
     ) {}
 
-
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        return this.userRepository.createUser(authCredentialsDto)
+    }
 
 }
