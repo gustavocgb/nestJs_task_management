c61ccf1fc45bef55eefebffc44a9729a520f3dab
Gustavo Barbosa | Eva Benefícios <gustavo.barbosa@evacard.com.br>
2022-11-16 10:39:56 -0300

    refactor: updateTaskStatus

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -37,11 +37,11 @@ export class TasksService {
         }
     }
 
-    // async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
-    //     const task = await this.getTaskById(id);
-    //     task.status = status;
-    //     await this.taskRepository.save(task);
-    //     return task;
-    // }
+    async updateTaskStatus(id: string, status: TaskStatus, user: User): Promise<Task> {
+        const task = await this.getTaskById(id, user);
+        task.status = status;
+        await this.taskRepository.save(task);
+        return task;
+    }
 
 }
